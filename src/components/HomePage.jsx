b5b2cee60111047a3601f5fcf30ad2cb7f6d4da6
Curtis J Pierce <Curtisjpierce39@gmail.com>
2025-02-04 18:49:48 -0800

    add initial description

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,7 +9,7 @@ function HomePage() {
             <p>This is the place to find all your needs at one click!</p>
             <img src="./src/components/img/e-commerce.jpg" className="img-fluid" alt="e-commerce image" /><br></br>
             <Button variant="primary" onClick={() => navigate(`/orders`)} className="me-2">Shop Now!</Button><br></br>
-            <a href="READme.md">Read Me!</a>
+            <a href="https://github.com/CurtisjPierce39/Mini-Project-E-commerce-API">Module 6 E-Commerce_API</a>
         </div>
         )
 }
